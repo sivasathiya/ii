f2cc18f5cbcb8e81bfe3ffb36821ebae1b4e7aba
sivasathiya <sathyasiva369@gmail.com>
2017-06-29 12:26:06 +0530

    Create arm.cpp

---
+++ arm.cpp
@@ -0,0 +1,24 @@
+#include<iosteam.h>
+#include<conio.h>
+int main()
+{
+int n,s,f=0,r;
+cout<<c;
+while(n!=0)
+{
+r=n%10;
+s=power(r,3);
+f=f+s;
+n=n/10;
+}
+cout<<f;
+if(f==n)
+{
+cout<<"armstrong";
+}
+else
+{
+cout<<"not";
+}
+result 0;
+}
